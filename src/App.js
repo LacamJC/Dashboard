26930caeca765dede9e3b7d0562811899d934f3a
LacamJC <joaoramajo744@gmail.com>
2025-03-14 13:29:15 -0300

    AOKD

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Dashboard from './dashboard/Dashboard';
 function App() {
   return (
     <>
+      <h1>Teste</h1>
       <Dashboard/>
     </>
   );
